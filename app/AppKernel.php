86aaa0260548bdd65669cba4232179442f3ec8ab
Administrateur <Administrateur@192.168.2.48>
2014-09-08 16:59:42 +0200

    theme bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Kali\Rest\UserBundle\KaliRestUserBundle(),
+            new Kali\Back\ThemeBundle\KaliBackThemeBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
