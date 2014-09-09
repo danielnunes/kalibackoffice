da70a006922e0fa0237afd8faf041b6f9ac74136
daniellaYamgoue <yamgoue.daniella@gmail.com>
2014-09-09 09:36:46 +0200

    config_dev

--- src/Kali/Back/UserBundle/Entity/User.php
+++ src/Kali/Back/UserBundle/Entity/User.php
@@ -18,6 +18,13 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    /**
+     * @var client
+     * 
+     * @ORM\OneToOne(targetEntity="Client", mappedBy="user")
+     */
+    private $client;
 
     /**
      * Constructor
@@ -35,5 +42,28 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set client
+     *
+     * @param \Kali\Back\UserBundle\Entity\Client $client
+     * @return User
+     */
+    public function setClient(\Kali\Back\UserBundle\Entity\Client $client = null)
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    /**
+     * Get client
+     *
+     * @return \Kali\Back\UserBundle\Entity\Client 
+     */
+    public function getClient()
+    {
+        return $this->client;
     }
 }
