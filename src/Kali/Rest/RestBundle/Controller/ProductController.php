947bde6cdd1bd276bddb9eb3242bbbd26532c600
Administrateur <Administrateur@192.168.2.48>
2014-09-10 15:36:04 +0200

    populaire produit

--- src/Kali/Rest/RestBundle/Controller/ProductController.php
+++ src/Kali/Rest/RestBundle/Controller/ProductController.php
@@ -3,35 +3,35 @@
 namespace Kali\Rest\RestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
  * Class ProductController
  * @package Kali\Rest\RestBundle\Controller
  */
-class ProductController extends Controller
-{
+class ProductController extends Controller {
+
     /**
      * Service Rest retournant tous les produits.
      * @return array
      */
-    public function getAllProductsAction()
-    {
+    public function getAllProductsAction() {
         $products = $this->getDoctrine()
-            ->getRepository("KaliBackProductBundle:Product")
-            ->findAll();
+                ->getRepository("KaliBackProductBundle:Product")
+                ->findAll();
 
         return $products;
     }
 
-    public function getFeaturedProductsAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery(
-            'SELECT * FROM KaliBackProductBundle:Product'
-        )->setMaxResults(10);
+    /**
+     * Service Rest retournant tous les produits.
+     * @return array
+     */
+    public function getFeatureProductsAction() {
+        $products = $this->getDoctrine()->getManager()
+                ->getRepository("KaliBackProductBundle:Product")
+                ->findPopular();
 
-        return $query;
+        return $products;
     }
+
 }
